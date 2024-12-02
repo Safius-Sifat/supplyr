194726e2aa3c6d5575b94a15acfe6f77a23a38d7
Safius-Sifat <safiussifat@gmail.com>
2024-12-02 19:33:41 +0600

    chore: fixed some issues

--- lib/src/features/auth/domain/app_user.dart
+++
@@ -1,7 +0,0 @@
-import 'package:drift/drift.dart';
-
-class AppUser extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get email => text()();
-  TextColumn get password => text()();
-}
